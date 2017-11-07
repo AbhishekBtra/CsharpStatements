a4a23085803e2491ef78a64cc23e242c443b85c6
JustGitt <33440640+JustGitt@users.noreply.github.com>
2017-11-07 10:14:58 +0530

    Arrays

---
+++ Statements/Statements/Arrays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statements
+{
+   public  class Aray
+    {
+        /*
+        You can store multiple variables of the same type in an array data structure. 
+        You declare an array by specifying the type of its elements.
+        type[] arrayName;
+        */
+        int[] integrArr;
+        public Aray()
+        {
+            integrArr = new int[] { 10,15,20,25,30};
+        }
+        public void ArrayIterator()
+        {
+            int dimension = integrArr.Rank; //Rank gets the dimension of the array .i.e 1 dimensional,2,3..and son on.
+            if(dimension == 1)
+            {
+                int cnt = 0;
+                foreach(int item in integrArr)
+                {
+                    Console.WriteLine("{0} Element of Array is {1}",cnt,item);
+                    cnt += 1;
+                }
+            }
+        }
+    }
+}
--- Statements/Statements/Program.cs
+++ Statements/Statements/Program.cs
@@ -10,6 +10,41 @@ namespace Statements
     {
         static void Main(string[] args)
         {
+            #region WhileLoop
+            /*WHILE - The while statement executes a statement or 
+                a block of statements until a specified expression evaluates to false. */
+            int variable = 0;
+            while (variable != 10) // at variable = 10 this will fail and loop exits
+            {
+                Console.WriteLine("Testing while {0} time(s)", variable);
+                variable += 1;
+            }
+
+            /*while with goto
+            The goto statement transfers the program control directly to a labeled statement.
+            A common use of goto is to transfer control to a specific
+            switch-case label or the default label in a switch statement.
+            The goto statement is also useful to get out of deeply nested loops.
+            */
+            int variable2 = 0;
+            while (variable2 < 10)
+            {
+                Console.WriteLine("Testing while with goto {0} time(s)", variable2);
+                if (variable2 == 5)
+                    goto finish;
+                variable2 += 1;
+            }
+
+        finish:
+            Console.WriteLine("The code ended");
+            #endregion
+
+            #region Arrays
+            Statements.Aray ob = new Aray();
+            ob.ArrayIterator();
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
